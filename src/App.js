aea08c6c11374165e983a8dfa81fcaf86e49cf22
Mohammad Gheydari <m.gheydari@flightio.com>
2018-08-11 17:15:32 +0430

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import Header from "./Components/Layouts/Header";
+// import Footer from "./Components/Layouts/Footer";
+import { Header, Footer } from "./Components/Layouts";
+import Exercises from "./Components/Exercises";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <Header />
+        <Exercises />
+        <Footer />
+      </React.Fragment>
     );
   }
 }
---
+++ src/Components/Exercises/index.jsx
@@ -0,0 +1,3 @@
+import React, { Component } from "react";
+
+export default props => <div>Hello From Exercises</div>;
